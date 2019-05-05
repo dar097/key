c0bd727811b00bf5652fe2debb8dcb5e676b08ab
unknown <Aquaterion@XPS15-9570.cloudlabs.int>
2019-05-06 00:07:59 +0200

    fix of change

--- index.js
+++ index.js
@@ -522,13 +522,12 @@ app.get('/projects/:id', (req, res) => {
     });
 });
 
-//Stages: Add
 app.post('/stages/create', isAuthed, (req, res) => {
     if(req.user && req.user._id){
         
         req.body = filter(req.body, 'name project start_date end_date details price');
 
-        UploadManyFiles(req, req.files ? Object.keys(req.files) : 0, 0, () => {//if images, file parameter needs to contain the word 'image'
+        UploadManyFiles(req, req.files ? Object.keys(req.files) : [], 0, () => {//if images, file parameter needs to contain the word 'image'
             Stages.create(req.body, (err, stage) => {
                 if(err || !stage)
                     res.status(400).send(err || 'Failed to create stage');
@@ -547,7 +546,7 @@ app.post('/stages/:id/edit', isAuthed, (req, res) => {
         
         req.body = filter(req.body, 'name project start_date end_date details price status');
 
-        UploadManyFiles(req, req.files ? Object.keys(req.files) : 0, 0, () => {//if images, file parameter needs to contain the word 'image'
+        UploadManyFiles(req, req.files ? Object.keys(req.files) : [], 0, () => {//if images, file parameter needs to contain the word 'image'
             Stages.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, stage) => {
                 if(err || !stage)
                     res.status(400).send(err || 'Failed to update stage');
@@ -574,19 +573,6 @@ app.post('/stages/:id/delete', isAuthed, (req, res) => {
         res.status(400).send({ error: 'Auth Error' });
     }
 });
-
-// app.get('/stages/:id', isAuthed, (req, res) => {
-//     if(req.user && req.user._id){
-//         Stages.findOne({ project: mongoose.Types.ObjectId(req.params.id) }).select('-__v').exec((err, stages) => {
-//             if(err)
-//                 res.status(400).send(err);
-//             else
-//                 res.status(200).send(appointments);
-//         });
-//     } else {
-//         res.status(400).send({ error: 'Auth Error' });
-//     }
-// });
 
 //Mongoose Connection
 mongoose.connect('mongodb+srv://keyadmin:nf2CY6GtMgG4XTX@keydb-ju9o2.mongodb.net/keydb', { 
